c6a417637adbc972a6a63c783580ab17425899b1
Shalom Kachko <shalom.kachko@mail.huji.ac.il>
2019-11-25 20:13:21 +0200

    advance and has more commands done

--- 07/VMtranslator.py
+++ 07/VMtranslator.py
@@ -12,22 +12,42 @@ class Parser:
         :param input_file: pu
         """
         self.file = input_file
+        self.lines = self.file.readlines()
         self.curr_line = None
+        self.line_counter = 0
+        self.command_dict = {
+            "add": "C_ARITHMETIC",
+            "sub": "C_ARITHMETIC",
+            "neg": "C_ARITHMETIC",
+            "eq": "C_ARITHMETIC",
+            "gt": "C_ARITHMETIC",
+            "lt": "C_ARITHMETIC",
+            "and": "C_ARITHMETIC",
+            "or": "C_ARITHMETIC",
+            "not": "C_ARITHMETIC",
+            "push": "C_PUSH",
+            "pop": "C_POP"
+        }
 
     def has_more_commands(self):
         """
 
         :return:
         """
-        next_line = self.file.readline()  # reads next line
-        if not next_line:
+        if self.line_counter > len(self.lines):  # means were done reading.
             return False
         return True
 
     def advance(self):
-        line = self.file.readline()
+        """
+
+        :return:
+        """
+        self.curr_line = self.lines[self.line_counter]
+        self.line_counter += 1
 
 
 if __name__ == '__main__':
     filename = sys.argv[1]
     file = open(filename)
+    print(file.next())
