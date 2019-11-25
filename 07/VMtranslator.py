3c35d148a7d45822e1d063caad0483f6aba9b455
Shalom Kachko <shalom.kachko@mail.huji.ac.il>
2019-11-25 20:52:09 +0200

    command type done

--- 07/VMtranslator.py
+++ 07/VMtranslator.py
@@ -34,7 +34,7 @@ class Parser:
 
         :return:
         """
-        if self.line_counter > len(self.lines):  # means were done reading.
+        if self.line_counter == len(self.lines):  # means were done reading.
             return False
         return True
 
@@ -46,8 +46,39 @@ class Parser:
         self.curr_line = self.lines[self.line_counter]
         self.line_counter += 1
 
+    def command_type(self):
+        """
+
+        :return:
+        """
+        temp_line = self.__clean_line()
+        return self.command_dict[temp_line[0]]
+
+    def __clean_line(self):
+        """
+
+        :return:
+        """
+        if len(self.curr_line.split()) > 1:
+            if not self.has_more_commands():
+                return self.curr_line.split(" ")
+            return self.curr_line[:-1].split(" ")
+        cleaned = ""
+        for letter in self.curr_line:
+            if letter == "/":
+                cleaned += ""
+                break
+            elif letter == "\n" or letter == " ":
+                cleaned += ""
+            else:
+                cleaned += letter
+        return [cleaned]
+
 
 if __name__ == '__main__':
     filename = sys.argv[1]
     file = open(filename)
-    print(file.next())
+    temp_P = Parser(file)
+    while temp_P.has_more_commands():
+        temp_P.advance()
+        print(temp_P.command_type())
