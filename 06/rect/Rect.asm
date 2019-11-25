b189af27802e479d0f657eb33acb425c4ce3df06
Shalom Kachko <shalom.kachko@mail.huji.ac.il>
2019-11-25 19:44:40 +0200

    1

--- 06/rect/Rect.asm
+++ 06/rect/Rect.asm
@@ -1,32 +1,35 @@
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
-// File name: projects/06/rect/RectL.asm
+// File name: projects/06/rect/Rect.asm
 
-// Symbol-less version of the Rect.asm program.
+// Draws a rectangle at the top-left corner of the screen.
+// The rectangle is 16 pixels wide and R0 pixels high.
 
-@0
-D=M
-@23
-D;JLE
-@16
-M=D
-@16384
-D=A
-@17
-M=D
-@17
-A=M
-M=-1
-@17
-D=M
-@32
-D=D+A
-@17
-M=D
-@16
-MD=M-1
-@10
-D;JGT
-@23
-0;JMP
+   @0
+   D=M
+   @INFINITE_LOOP
+   D;JLE 
+   @counter
+   M=D
+   @SCREEN
+   D=A
+   @address
+   M=D
+(LOOP)
+   @address
+   A=M
+   M=-1
+   @address
+   D=M
+   @32
+   D=D+A
+   @address
+   M=D
+   @counter
+   MD=M-1
+   @LOOP
+   D;JGT
+(INFINITE_LOOP)
+   @INFINITE_LOOP
+   0;JMP
--- 07/VMtranslator.py
+++ 07/VMtranslator.py
@@ -2,5 +2,32 @@ import sys, os
 
 
 class Parser:
-    def __init__(self):
-        pass
+    """
+
+    """
+
+    def __init__(self, input_file):
+        """
+
+        :param input_file: pu
+        """
+        self.file = input_file
+        self.curr_line = None
+
+    def has_more_commands(self):
+        """
+
+        :return:
+        """
+        next_line = self.file.readline()  # reads next line
+        if not next_line:
+            return False
+        return True
+
+    def advance(self):
+        line = self.file.readline()
+
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    file = open(filename)
